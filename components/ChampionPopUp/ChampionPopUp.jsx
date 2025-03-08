4e9d65010cbc583caa3726dfad31889e4c607547
Mr Poorya Khoshbin <www.pooryakhoshbin@gmail.com>
2025-03-08 10:46:09 +0430

    the porblem with champios popup solved

--- components/ChampionPopUp/ChampionPopUp.jsx
+++ components/ChampionPopUp/ChampionPopUp.jsx
@@ -54,7 +54,7 @@ export default function ChampionPopUp() {
     >
       {isVisible && (
         <div
-          className={`flex flex-col items-center fixed z-50 w-full md:w-600px xl:w-900px border-4 border-black p-1 bg-white ${
+          className={`flex flex-col items-center fixed z-50 w-full md:w-600px xl:w-600px border-4 border-black p-1 bg-white ${
             showPopUp ? 'top-5' : '-top-100%'
           } transition-all duration-1000`}
         >
@@ -64,11 +64,11 @@ export default function ChampionPopUp() {
               onClick={() => setShowPopUp(false)}
             />
           </div>
-          <div className="w-full px-5">
-            <div className="rtl w-full text-footerBtn font-common-heavy text-25px md:text-30px xl:text-50px">
+          <div className="w-full px-5 flex flex-col items-center">
+            <div className="rtl w-full text-footerBtn font-common-heavy text-25px md:text-30px">
               برنده مسابقه داستان نویسی آوای زریاب
             </div>
-            <div className="w-full relative h-200px md:h-280px xl:h-430px mt-1">
+            <div className="w-full relative h-200px md:h-280px mt-1">
               {data?.featured_image ? (
                 <Image
                   src={data?.featured_image}
@@ -78,31 +78,31 @@ export default function ChampionPopUp() {
                   className="absolute"
                 />
               ) : (
-                <div className="w-full h-full flex justify-center items-center">
+                <div className="w-full  h-full flex justify-center items-center">
                   the champion image not found
                 </div>
               )}
             </div>
-            <div className="w-full font-new-black text-30px xl:text-50px rtl mt-1">
+            <div className="w-full font-new-black text-30px rtl mt-1">
               {data?.story.title}
             </div>
-            <div className="w-full font-common-heavy text-16px text-footerBtn xl:text-25px rtl mt-1 flex">
+            <div className="w-full font-common-heavy text-16px text-footerBtn rtl mt-1 flex">
               <div>نویسنده:</div>
-              <div className="mr-1">{data?.author.name}</div>
+              <div className="mr-1">{data?.author?.name}</div>
             </div>
-            <div className="w-full rtl font-common-regular text-10px md:text-14px xl:text-18px mt-1">
+            <div className="w-full rtl font-common-regular text-10px md:text-14px mt-1">
               {data?.story.excerpt}
             </div>
             <div className="w-full flex flex-row-reverse justify-between mt-3 mb-3">
               <Link
-                href={`/literarywritings/story/${data?.story.slug}`}
-                className="flex justify-center items-center font-common-heavy text-18px px-7 py-2 md:px-12 md:text-20px md:py-3 xl:px-20 xl:py-5 xl:text-25px bg-black border-2 border-black text-white"
+                href={`/literarywritings/story/${data?.story?.slug}`}
+                className="flex justify-center items-center font-common-heavy text-18px px-7 py-2 md:px-12 md:text-20px md:py-3 bg-black border-2 border-black text-white"
               >
                 خواندن داستان
               </Link>
               <Link
-                href={`/authors/${data?.author.slug}`}
-                className="flex justify-center items-center font-common-heavy text-18px px-7 py-2 md:px-12 md:text-20px md:py-3 xl:px-20 xl:py-5 xl:text-25px bg-white border-2 border-footerBtn text-footerBtn"
+                href={`/authors/${data?.author?.slug}`}
+                className="flex justify-center items-center font-common-heavy text-18px px-7 py-2 md:px-12 md:text-20px md:py-3 bg-white border-2 border-footerBtn text-footerBtn"
               >
                 درباره نویسنده
               </Link>
