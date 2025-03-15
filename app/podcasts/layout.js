698a060e7d191fc8d21193a91be6af61a40e2d62
Mr Poorya Khoshbin <www.pooryakhoshbin@gmail.com>
2025-03-15 11:52:10 +0430

    the podcast page metadata added

---
+++ app/podcasts/layout.js
@@ -0,0 +1,79 @@
+import React from 'react';
+
+export const metadata = {
+  title: 'کتاب های صوتی',
+  description: 'کتاب های صوتی آوای زریاب',
+  openGraph: {
+    title: 'کتاب های صوتی',
+    description: 'کتاب های صوتی آوای زریاب',
+    url: 'https://zaryb3.vercel.app',
+    siteName: 'وبسایت ادبی آوای زریاب',
+    images: [
+      {
+        url: 'https://images.pexels.com/photos/1831744/pexels-photo-1831744.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        width: 1129,
+        height: 750,
+        alt: 'کتاب های صوتی',
+      },
+    ],
+    locale: 'fa_IR',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: '@your_twitter_handle',
+    title: 'کتاب های صوتی',
+    description: 'کتاب های صوتی آوای زریاب',
+    images: [
+      'https://images.pexels.com/photos/1831744/pexels-photo-1831744.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    ],
+  },
+  viewport: 'width=device-width, initial-scale=1.0',
+  robots: {
+    index: true,
+    follow: true,
+  },
+
+  alternates: {
+    canonical: 'https://zaryb3.vercel.app',
+  },
+
+  keywords: [
+    'ادبیات',
+    'شعر',
+    'داستان',
+    'رمان',
+    'نثر',
+    'نقد ادبی',
+    'سبک نوشتاری',
+    'متن ادبی',
+    'شعر معاصر',
+    'ادبیات کلاسیک',
+    'نویسندگی خلاق',
+    'تحلیل داستان',
+    'روایت‌پردازی',
+    'سبک‌های ادبی',
+    'کتاب‌خوانی',
+    'معرفی کتاب',
+    'بهترین کتاب‌های ادبی',
+    'آموزش نویسندگی',
+    'الهام برای نویسندگی',
+    'جملات زیبا',
+    'نقل‌قول‌های ادبی',
+    'متن‌های عاشقانه',
+    'متن‌های انگیزشی',
+  ],
+
+  authors: [
+    {
+      name: 'Cyborg Tech Creative Agency',
+      url: 'https://portfolio-poorya.vercel.app/',
+    },
+  ],
+
+  manifest: '/site.webmanifest',
+};
+
+export default function AboutPageLayout({ children }) {
+  return <div>{children}</div>;
+}
