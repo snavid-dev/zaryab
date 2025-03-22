be2bd70680c1dd2abc45e21da63f845f6045c40e
Mr Poorya Khoshbin <www.pooryakhoshbin@gmail.com>
2025-03-22 11:27:02 +0430

    anoter issu solved

--- components/Heading2/Heading2.jsx
+++ components/Heading2/Heading2.jsx
@@ -4,7 +4,6 @@ export default function Heading2({ title }) {
   return (
     <div className="inline  relative">
       <p className="font-common-heading2 inline m-0 p-0">{title}</p>
-      <div className="w-full bg-[#FDF0DF] absolute h-[30%] bottom-[20%] -z-10"></div>
     </div>
   );
 }
